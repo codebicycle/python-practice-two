3639992042e62af8201b8f0d8fe3c06e2ce0f42c
codebicycle <codebicycle@gmail.com>
2016-11-02 18:30:28 +0200

    Refactor config dict

--- config_dict.py
+++ config_dict.py
@@ -2,20 +2,25 @@
 
 """
 from pprint import pprint
+import os
 
 class ConfigDict(dict):
 
     def __init__(self, filepath):
-        self.filepath = filepath
-        with open(filepath, 'r') as f:
-            for line in f:
-                key, value = line.split('=')
-                value = value.strip()
-                dict.__setitem__(self, key, value)
+
+        self._filepath = filepath
+        if os.path.isfile(filepath):
+            with open(filepath, 'r') as f:
+                for line in f:
+                    line = line.strip()
+                    key, value = line.split('=', 1)
+                    dict.__setitem__(self, key, value)
+
 
     def __setitem__(self, key, value):
+
         dict.__setitem__(self, key, value)
-        with open(self.filepath, 'w') as f:
+        with open(self._filepath, 'w') as f:
             it = ('{}={}\n'.format(key, value) for key, value in self.items())
             f.writelines(it)
 
