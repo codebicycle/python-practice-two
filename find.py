0eb385fa6acd6e3b1aab3bae610b3e6ff88a02db
codebicycle <codebicycle@gmail.com>
2016-11-19 12:49:51 +0200

    Pass find.py test

--- find.py
+++ find.py
@@ -6,7 +6,24 @@ find.py unix find-like utility
 Usage: find.py DIRECTORY -name PATTERN
 
 """
-
+import os
 import subprocess
+import sys
+from fnmatch import fnmatch
 
-subprocess.run(['find', '.', '-name', '*.txt'])
+# subprocess.run(['find', '.', '-name', '*.txt'])
+
+
+def main():
+    start = sys.argv[1]
+    pattern = sys.argv[3]
+
+    for dirpath, dirnames, filenames in os.walk(start):
+        for filename in filenames:
+            if fnmatch(filename, pattern):
+                filepath = os.path.join(dirpath, filename)
+                print(filepath)
+
+
+if __name__ == '__main__':
+    main()
