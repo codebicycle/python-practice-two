ccca453a25f903466f80de49b355df4fd645db76
codebicycle <codebicycle@gmail.com>
2016-11-23 18:47:46 +0200

    Add poker ranking

--- poker.py
+++ poker.py
@@ -1,6 +1,17 @@
 import random
 from copy import copy
 
+ROYAL_FLUSH = 10
+STRAIGHT_FLUSH = 9
+FOUR_OF_A_KIND = 8
+FULL_HOUSE = 7
+FLUSH = 6
+STRAIGHT = 5
+THREE_OF_A_KIND = 4
+TWO_PAIRS = 3
+PAIR = 2
+HIGH_CARD = 1
+
 
 class Card:
     SUIT_DICT = {
@@ -21,7 +32,7 @@ class Card:
         self.suit = suit.strip().upper()
 
     @classmethod
-    def from_touple(cls, rank, suit):
+    def from_tuple(cls, rank, suit):
         obj = cls.__new__(cls)  # Does not call __init__
         obj.rank = rank.strip().upper()
         obj.suit = suit.strip().upper()
@@ -37,8 +48,7 @@ class Card:
 def create_deck():
     ranks = Card.RANKS
     suits = Card.SUITS
-
-    return [Card.from_touple(r, s) for r in ranks for s in suits]
+    return [Card.from_tuple(r, s) for r in ranks for s in suits]
 
 
 def deal(num_hands, cards_per_hand=5, deck=None):
@@ -49,3 +59,83 @@ def deal(num_hands, cards_per_hand=5, deck=None):
         hand = [deck.pop() for j in range(cards_per_hand)]
         hands.append(hand)
     return hands
+
+
+def ranks(hand):
+    """Rank values sorted from highest to lowest"""
+    rank_values = [card.rank_value() for card in hand]
+    rank_values.sort(reverse=True)
+
+    # Ace low straight
+    if [14, 5, 4, 3, 2] == rank_values:
+        return [5, 4, 3, 2, 1]
+
+    return rank_values
+
+
+def is_ace_high(hand):
+    rank_values = ranks(hand)
+    return 14 == rank_values[0]
+
+
+def is_straight(hand):
+    rank_values = ranks(hand)
+
+    for i in range(1, len(rank_values)):
+        current = rank_values[i]
+        previous = rank_values[i - 1]
+        if current != previous - 1:
+            return False
+    return True
+
+
+def is_flush(hand):
+    suits = {card.suit for card in hand}
+    return len(suits) == 1
+
+
+def is_straight_flush(hand):
+    return is_straight(hand) and is_flush(hand)
+
+
+def is_royal_flush(hand):
+    return is_straight_flush(hand) and is_ace_high(hand)
+
+
+def poker_rank(hand):
+    count = {}
+    rank_values = ranks(hand)
+    for value in rank_values:
+        count[value] = count.get(value, 0) + 1
+
+    frequencies = sorted(count.values(), reverse=True)
+    frequencies = tuple(frequencies)
+
+    card_ranks = sorted(count, key=(lambda k: (count[k], k)), reverse=True)
+    card_ranks = tuple(card_ranks)
+
+    return frequencies, card_ranks
+
+
+def ranking(hand):
+    hand_rank = poker_rank(hand)
+    frequencies, _values = hand_rank
+    return (
+        ROYAL_FLUSH if is_royal_flush(hand) else
+        STRAIGHT_FLUSH if is_straight_flush(hand) else
+        FOUR_OF_A_KIND if (4, 1) == frequencies else
+        FULL_HOUSE if (3, 2) == frequencies else
+        FLUSH if is_flush(hand) else
+        STRAIGHT if is_straight(hand) else
+        THREE_OF_A_KIND if (3, 1, 1) == frequencies else
+        TWO_PAIRS if (2, 2, 1) == frequencies else
+        PAIR if (2, 1, 1, 1) == frequencies else
+        HIGH_CARD), hand_rank
+
+
+def build_hand(hand_str):
+    """
+    :param hand_str: '9H 2C AH 10D 4H'
+    :return: list of Cards
+    """
+    return [Card(card_str) for card_str in hand_str.split()]
--- test_poker.py
+++ test_poker.py
@@ -1,4 +1,4 @@
-from poker import create_deck, deal, Card
+from poker import *
 
 
 def test_create_deck():
@@ -18,12 +18,12 @@ def test_deal():
 
 def test_card_equivalent_initializers():
     card1 = Card('2H')
-    card2 = Card.from_touple('2', 'H')
+    card2 = Card.from_tuple('2', 'H')
     assert repr(card1) == repr(card2), (
         "Equivalent initializers should give the same representation")
 
 
-def test_ranks():
+def test_card_rank_value():
     queen_of_spades = Card('QS')
     ace_of_diamonds = Card('AD')
     deuce_of_hearts = Card('2H')
@@ -33,3 +33,72 @@ def test_ranks():
     assert 14 == ace_of_diamonds.rank_value()
     assert 2 == deuce_of_hearts.rank_value()
     assert 10 == ten_of_clubs.rank_value()
+
+
+def test_ranks():
+    hand = build_hand('2H QS AD 10C 10H')
+    result = ranks(hand)
+    expect = [14, 12, 10, 10, 2]
+    assert expect == result
+
+
+def test_is_staight():
+    not_straight = build_hand('2H QS AD 10C 10H')
+    straight = build_hand('7H 8S 9D 10C JH')
+    ace_low_straight = build_hand('2H 3S 4D 5C AH')
+    ace_high_straight = build_hand('AH KS QD JC 10H')
+
+    assert is_straight(not_straight) is False
+    assert is_straight(straight) is True
+    assert is_straight(ace_low_straight) is True
+    assert is_straight(ace_high_straight) is True
+
+
+def test_is_flush():
+    not_flush = build_hand('2H QH AH 10H 10D')
+    flush = build_hand('7H 8H 9H 10H JH')
+
+    assert is_flush(not_flush) is False
+    assert is_flush(flush) is True
+
+
+def test_poker_rank():
+    high_card = build_hand('10C 9D JH 2H KS')
+    pair = build_hand('7C 10C 2C 10D JC')
+    two_pairs = build_hand('5C 5D 2C JC JD')
+    three_of_a_kind = build_hand('5C 5D 5H JC 9D')
+    full_house = build_hand('5C 5D 5H JC JD')
+    four_of_a_kind = build_hand('5C 5D 5H 5S JD')
+
+    assert ((1, 1, 1, 1, 1), (13, 11, 10, 9, 2)) == poker_rank(high_card)
+    assert ((2, 1, 1, 1), (10, 11, 7, 2)) == poker_rank(pair)
+    assert ((2, 2, 1), (11, 5, 2)) == poker_rank(two_pairs)
+    assert ((3, 1, 1), (5, 11, 9)) == poker_rank(three_of_a_kind)
+    assert ((3, 2), (5, 11)) == poker_rank(full_house)
+    assert ((4, 1), (5, 11)) == poker_rank(four_of_a_kind)
+
+
+def test_ranking():
+    high_card = build_hand('10C 9D JH 2H KS')
+    pair = build_hand('7C 10C 2C 10D JC')
+    two_pairs = build_hand('5C 5D 2C JC JD')
+    three_of_a_kind = build_hand('5C 5D 5H JC 9D')
+    straight = build_hand('2C 3H 4H 5D 6C')
+    flush = build_hand('9D JD AD 2D 5D')
+    full_house = build_hand('5C 5D 5H JC JD')
+    four_of_a_kind = build_hand('5C 5D 5H 5S JD')
+    straight_flush = build_hand('4C 5C 6C 7C 8C')
+    straight_flush_ace_low = build_hand('AC 2C 3C 4C 5C')
+    royal_flush = build_hand('AS QS KS 10S JS')
+
+    assert HIGH_CARD == ranking(high_card)[0]
+    assert PAIR == ranking(pair)[0]
+    assert TWO_PAIRS == ranking(two_pairs)[0]
+    assert THREE_OF_A_KIND == ranking(three_of_a_kind)[0]
+    assert STRAIGHT == ranking(straight)[0]
+    assert FLUSH == ranking(flush)[0]
+    assert FULL_HOUSE == ranking(full_house)[0]
+    assert FOUR_OF_A_KIND == ranking(four_of_a_kind)[0]
+    assert STRAIGHT_FLUSH == ranking(straight_flush)[0]
+    assert STRAIGHT_FLUSH == ranking(straight_flush_ace_low)[0]
+    assert ROYAL_FLUSH == ranking(royal_flush)[0]
