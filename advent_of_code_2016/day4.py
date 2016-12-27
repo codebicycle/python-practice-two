037fc6fb2d1bac7bff97935b62cf9483cbc89618
codebicycle <codebicycle@gmail.com>
2016-12-28 00:11:57 +0200

    Day 4: Security Through Obscurity, Part 2

--- advent_of_code_2016/day4.py
+++ advent_of_code_2016/day4.py
@@ -16,11 +16,23 @@ Of the real rooms from the list above, the sum of their sector IDs is 1514.
 
 What is the sum of the sector IDs of the real rooms?
 
+--- Part Two ---
+
+With all the decoy data out of the way, it's time to decrypt this list and get moving.
+
+The room names are encrypted by a state-of-the-art shift cipher, which is nearly unbreakable without the right software. However, the information kiosk designers at Easter Bunny HQ were not expecting to deal with a master cryptographer like yourself.
+
+To decrypt a room name, rotate each letter forward through the alphabet a number of times equal to the room's sector ID. A becomes B, B becomes C, Z becomes A, and so on. Dashes become spaces.
+
+For example, the real name for qzmt-zixmtkozy-ivhz-343 is very encrypted name.
+
+What is the sector ID of the room where North Pole objects are stored?
+
 """
 
 import re
-
 import collections
+import string
 
 LETTERS_RE = re.compile(r'([-a-z]*)-')
 DIGITS_RE = re.compile(r'\b(\d+)\b')
@@ -55,17 +67,31 @@ def is_valid(name, checksum):
     return checksum == computed_checksum
 
 
+def decode(name, sectorid):
+    alphabet = string.ascii_lowercase
+    mapping = {char: alphabet[(idx + sectorid) % 26]
+               for idx, char in enumerate(alphabet)}
+    mapping['-'] = ' '
+    table = str.maketrans(mapping)
+    return name.translate(table)
+
+
 def main():
     with open('input4.txt', 'r') as f:
         lines = f.readlines()
 
     total = 0
+    northpole_sectorid = None
     for line in lines:
         name, sectorid, checksum = parse(line)
         if is_valid(name, checksum):
             total += sectorid
+            cleartext_name = decode(name, sectorid)
+            if cleartext_name == 'northpole object storage':
+                northpole_sectorid = sectorid
 
     print(total)
+    print(northpole_sectorid)
 
 
 if __name__ == '__main__':
