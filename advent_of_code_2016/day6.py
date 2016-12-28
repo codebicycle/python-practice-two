110570be81c33a43acea92a383b14f01ad869e0e
codebicycle <codebicycle@gmail.com>
2016-12-28 13:34:07 +0200

    Day 6: Signals and Noise, Part 2

--- advent_of_code_2016/day6.py
+++ advent_of_code_2016/day6.py
@@ -6,11 +6,20 @@ def main():
         lines = f.read().splitlines()
 
     columns = zip(*lines)
-    most_common = [collections.Counter(column).most_common(1)[0][0]
-                   for column in columns]
+    frequency_columns = [collections.Counter(column).most_common()
+                         for column in columns]
 
-    message = ''.join(most_common)
-    print(message)
+    most_common = [lst[0] for lst in frequency_columns]
+    most_common_elements = [tup[0] for tup in most_common]
+
+    least_common = [lst[-1] for lst in frequency_columns]
+    least_common_elements = [tup[0] for tup in least_common]
+
+    first_message = ''.join(most_common_elements)
+    print(first_message)
+
+    second_message = ''.join(least_common_elements)
+    print(second_message)
 
 
 if __name__ == '__main__':
