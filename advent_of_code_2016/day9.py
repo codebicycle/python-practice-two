06e2525f70a07b2534c7aa5c821e24b957e8bc68
codebicycle <codebicycle@gmail.com>
2016-12-30 10:49:19 +0200

    Day 9: Explosives in Cyberspace, Part 2 too slow for input

--- advent_of_code_2016/day9.py
+++ advent_of_code_2016/day9.py
@@ -18,13 +18,31 @@ For example:
 
 What is the decompressed length of the file (your puzzle input)? Don't count whitespace.
 
+--- Part Two ---
+
+Apparently, the file actually uses version two of the format.
+
+In version two, the only difference is that markers within decompressed data are decompressed. This, the documentation explains, provides much more substantial compression capabilities, allowing many-gigabyte files to be stored in only a few kilobytes.
+
+For example:
+
+    (3x3)XYZ still becomes XYZXYZXYZ, as the decompressed section contains no markers.
+    X(8x2)(3x3)ABCY becomes XABCABCABCABCABCABCY, because the decompressed data from the (8x2) marker is then further decompressed, thus triggering the (3x3) marker twice for a total of six ABC sequences.
+    (27x12)(20x12)(13x14)(7x10)(1x12)A decompresses into a string of A repeated 241920 times.
+    (25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN becomes 445 characters long.
+
+Unfortunately, the computer you brought probably doesn't have enough memory to actually decompress the file; you'll have to come up with another way to get its decompressed length.
+
+What is the decompressed length of the file using this improved format?
+
 """
+import copy
 import re
 
 MARKER = re.compile(r'\((\d+)x(\d+)\)')
 
 
-def parse(content, message):
+def parse_recursive(content, message):
     match = MARKER.search(content)
     if match:
         length = int(match.group(1))
@@ -35,28 +53,90 @@ def parse(content, message):
         message.append(content[:start])
         message.append(content[end:end + length] * multiplier)
         content = content[end + length:]
-        parse(content, message)
+        parse_recursive(content, message)
     else:
         message.append(content)
 
 
-def decode_message(content):
-    content = re.sub(r'\s', '', content)
+def decode(content):
+    # content = re.sub(r'\s', '', content)
 
     accumulator = []
-    parse(content, accumulator)
+    parse_recursive(content, accumulator)
     message = ''.join(accumulator)
     return message
 
 
+def decode_deep(content):
+    representation = build_representation(content)
+
+    clone = copy.deepcopy(representation)
+    while any(isinstance(i, list) for i in clone):
+        clone = expand_representation(clone)
+
+    message = ''.join(clone)
+    return message, len(message)
+
+
+def build_representation(content):
+    if not content:
+        return []
+
+    representation = []
+    match = MARKER.search(content)
+    if match:
+        length = int(match.group(1))
+        multiplier = int(match.group(2))
+        start = match.start()
+        end = match.end()
+
+        if start != 0:
+            representation.append(build_representation(content[:start]))
+
+        if multiplier:
+            inside = [multiplier,
+                      build_representation(content[end: end + length])]
+            representation.append(inside)
+
+        if end + length != match.endpos:
+            representation.extend(build_representation(content[end + length:]))
+    else:
+        representation.append(content)
+
+    return representation
+
+
+def expand_representation(array):
+    hold = []
+    for item in array:
+
+        if isinstance(item, list):
+            if len(item) == 1:
+                hold.append(item[0])
+            elif any(isinstance(i, list) for i in item):
+                hold.append(expand_representation(item))
+            elif isinstance(item[0], int):
+                multiplier = item[0]
+                hold.extend([multiplier * i for i in item[1:]])
+            else:
+                hold.extend(item[:])
+        else:
+            hold.append(item)
+
+    return hold
+
+
 def main():
     with open('input9.txt', 'r') as f:
-        content = f.read()
+        content = f.read().strip()
 
-    message = decode_message(content)
+    message = decode(content)
     length = len(message)
     print(length)
 
+    # message, length = decode_deep(content)
+    # print(message[:255])
+    # print(length)
 
 if __name__ == '__main__':
     main()
--- advent_of_code_2016/test_day9.py
+++ advent_of_code_2016/test_day9.py
@@ -1,9 +1,56 @@
-from advent_of_code_2016.day9 import parse
+from advent_of_code_2016.day9 import parse_recursive, decode, decode_deep
 
 
-def test_parse():
+def test_parse_recursive():
     content = 'aaa(2x3)xyzzz'
     message = []
-    parse(content, message)
+    parse_recursive(content, message)
     expected = ['aaa', 'xyxyxy', 'zzz']
     assert expected == message
+
+
+def test_decode():
+    result = decode('ADVENT')
+    expected = 'ADVENT'
+    assert expected == result
+
+    result = decode('A(1x5)BC')
+    expected = 'ABBBBBC'
+    assert expected == result
+
+    result = decode('(3x3)XYZ')
+    expected = 'XYZXYZXYZ'
+    assert expected == result
+
+    result = decode('A(2x2)BCD(2x2)EFG')
+    expected = 'ABCBCDEFEFG'
+    assert expected == result
+
+    result = decode('(6x1)(1x3)A')
+    expected = '(1x3)A'
+    assert expected == result
+
+    result = decode('X(8x2)(3x3)ABCY')
+    expected = 'X(3x3)ABC(3x3)ABCY'
+    assert expected == result
+
+
+def test_decode_deep():
+    content = '(3x3)XYZ'
+    message, length = decode_deep(content)
+    expected = 'XYZXYZXYZ'
+    assert expected == message
+
+    content = 'X(8x2)(3x3)ABCY'
+    message, length = decode_deep(content)
+    expected = 'XABCABCABCABCABCABCY'
+    assert expected == message
+
+    content = '(27x12)(20x12)(13x14)(7x10)(1x12)A'
+    message, length = decode_deep(content)
+    expected = 'A' * 241920
+    assert expected == message
+
+    content = '(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN'
+    message, length = decode_deep(content)
+    assert 445 == length
