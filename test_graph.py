5ddb96c0d4e664dd507047e5d5c59a275f0a1e26
codebicycle <codebicycle@gmail.com>
2017-01-05 13:04:29 +0200

    Add graph search

---
+++ test_graph.py
@@ -0,0 +1,26 @@
+import pytest
+
+from advent_of_code_2016.graph import (find_path, find_all_paths,
+                                       find_shortest_path)
+
+
+@pytest.fixture
+def graph():
+    return {'A': ['B', 'C'],
+            'B': ['C', 'D'],
+            'C': ['D'],
+            'D': ['C'],
+            'E': ['F'],
+            'F': ['C'], }
+
+
+def test_find_path(graph):
+    pass
+
+
+def test_find_all_paths(graph):
+    pass
+
+
+def test_find_shortest_path(graph):
+    pass
