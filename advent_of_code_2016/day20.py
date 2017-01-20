755058b6ed29465f463f294187021a941eaded64
codebicycle <codebicycle@gmail.com>
2017-01-20 11:24:42 +0200

    Day 20: Firewall Rules, Part 2

--- advent_of_code_2016/day20.py
+++ advent_of_code_2016/day20.py
@@ -15,16 +15,17 @@ The blacklist specifies ranges of IPs (inclusive of both the start and end value
 
 Given the list of blocked IPs you retrieved from the firewall (your puzzle input), what is the lowest-valued IP that is not blocked?
 
+--- Part Two ---
+
+How many IPs are allowed by the blacklist?
+
 """
 import re
 
 RE = re.compile(r'(\d+)-(\d+)')
 
 
-def main():
-    with open('input20.txt', 'r') as f:
-        lines = f.readlines()
-
+def parse(lines):
     intervals = []
     for line in lines:
         match = RE.match(line)
@@ -33,17 +34,69 @@ def main():
         end = int(match.group(2))
         intervals.append((start, end))
 
-    intervals.sort()
+    return intervals
 
+
+def group_intervals(intervals):
+    ordered_intervals = sorted(intervals)
+    accumulator = []
+
+    previous_start, previous_end = ordered_intervals[0]
+    for start, end in ordered_intervals:
+        if start <= previous_end + 1:
+            if end > previous_end:
+                previous_end = end
+        else:
+            accumulator.append((previous_start, previous_end))
+            previous_start, previous_end = start, end
+
+    if accumulator[-1] != (previous_start, previous_end):
+        accumulator.append((previous_start, previous_end))
+
+    return accumulator
+
+
+def find_lowest_ip(intervals):
     first_interval_start, first_interval_end = intervals[0]
+    if first_interval_start != 0:
+        lowest = 0
+    else:
+        lowest = first_interval_end + 1
+
+    return lowest
+
+
+def count_allowed_ips(intervals, max_limit):
+    previous_start, previous_end = intervals[0]
+    counter = previous_start - 0
 
     for start, end in intervals[1:]:
-        if start <= first_interval_end + 1:
-            if end > first_interval_end:
-                first_interval_end = end
-        else:
-            print(first_interval_end + 1)
+        if start >= max_limit:
+            counter += max_limit - (previous_end + 1)
             break
+
+        counter += start - (previous_end + 1)
+
+        if end >= max_limit:
+            break
+
+        previous_start, previous_end = start, end
+    return counter
+
+
+def main():
+    with open('input20.txt', 'r') as f:
+        lines = f.readlines()
+
+    raw_intervals = parse(lines)
+    intervals = group_intervals(raw_intervals)
+
+    lowest = find_lowest_ip(intervals)
+    print('Lowest-valued IP is', lowest)
+
+    max_limit = 4294967295
+    allowed = count_allowed_ips(intervals, max_limit)
+    print(allowed, 'allowed IPs')
 
 
 if __name__ == '__main__':
