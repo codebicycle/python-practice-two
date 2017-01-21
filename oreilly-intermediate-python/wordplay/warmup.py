9d3bac0f3c5d9d5f1302f3e1d765e70323b8375b
codebicycle <codebicycle@gmail.com>
2017-01-21 22:48:49 +0200

    wordplay, words containing all vowels

--- oreilly-intermediate-python/wordplay/warmup.py
+++ oreilly-intermediate-python/wordplay/warmup.py
@@ -4,8 +4,15 @@ from pprint import pprint
 import scrabble
 
 
-def main():
+def words_with_all_vowels(wordlist):
+    vowels = set('aeiou')
+    words = [word
+             for word in wordlist
+             if vowels.issubset(set(word))]
+    return words
 
+
+def main():
     uu = [word
           for word in scrabble.wordlist
           if 'uu' in word]
@@ -25,6 +32,11 @@ def main():
     print()
     print('Letters that do not appear doubled in words:')
     print(', '.join(letters))
+
+    print()
+    print('Words containing all vowels:')
+    for word in words_with_all_vowels(scrabble.wordlist):
+        print(word)
 
 if __name__ == '__main__':
     main()
