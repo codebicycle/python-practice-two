a77abc0b1aed6227fd818056e53409b979df3983
codebicycle <codebicycle@gmail.com>
2017-01-21 22:30:36 +0200

    wordplay, no double letters

--- oreilly-intermediate-python/wordplay/warmup.py
+++ oreilly-intermediate-python/wordplay/warmup.py
@@ -1,3 +1,4 @@
+import string
 from pprint import pprint
 
 import scrabble
@@ -13,6 +14,17 @@ def main():
     for word in uu:
         print(word)
 
+    # Letter that do not appear doubled in words
+    letters = set(string.ascii_lowercase)
+    for letter in letters.copy():
+        for word in scrabble.wordlist:
+            if letter + letter in word:
+                letters.remove(letter)
+                break
+
+    print()
+    print('Letters that do not appear doubled in words:')
+    print(', '.join(letters))
 
 if __name__ == '__main__':
     main()
