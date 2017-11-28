a362146420d6bea80061068700cc841b85b2f634
codebicycle <codebicycle@gmail.com>
2017-11-28 17:42:18 +0200

    Decrease nesting

--- gilded_rose.py
+++ gilded_rose.py
@@ -10,13 +10,14 @@ class GildedRose(object):
             self._update_helper(item)
 
     def _update_helper(self, item):
-        if item.name == "Aged Brie" or item.name == "Backstage passes to a TAFKAL80ETC concert":
+        if item.name == "Aged Brie":
             increase_quality(item)
-            if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                if item.sell_in <= 10:
-                    increase_quality(item)
-                if item.sell_in <= 5:
-                    increase_quality(item)
+        elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+            increase_quality(item)
+            if item.sell_in <= 10:
+                increase_quality(item)
+            if item.sell_in <= 5:
+                increase_quality(item)
         else:
             if item.quality > 0:
                 if item.name != "Sulfuras, Hand of Ragnaros":
