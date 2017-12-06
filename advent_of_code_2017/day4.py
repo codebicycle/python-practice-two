e3d25b35d374ad5fb286a3b63f8c97eba86edcd2
codebicycle <codebicycle@gmail.com>
2017-12-06 23:34:49 +0200

    AoC 2017, Day 4: High-Entropy Passphrases - part 2

--- advent_of_code_2017/day4.py
+++ advent_of_code_2017/day4.py
@@ -12,17 +12,35 @@ aa bb cc dd aa is not valid - the word aa appears more than once.
 aa bb cc dd aaa is valid - aa and aaa count as different words.
 The system's full passphrase list is available as your puzzle input. How many passphrases are valid?
 
+--- Part Two ---
+
+For added security, yet another system policy has been put in place. Now, a valid passphrase must contain no two words that are anagrams of each other - that is, a passphrase is invalid if any word's letters can be rearranged to form any other word in the passphrase.
+
+For example:
+
+abcde fghij is a valid passphrase.
+abcde xyz ecdab is not valid - the letters from the third word can be rearranged to form the first word.
+a ab abc abd abf abj is a valid passphrase, because all letters need to be used when forming another word.
+iiii oiii ooii oooi oooo is valid.
+oiii ioii iioi iiio is not valid - any of these words can be rearranged to form any other word.
+Under this new system policy, how many passphrases are valid?
+
 """
 
-def count_passphrases(puzzle_input):
+def count_passphrases(puzzle_input, validator):
     count = 0
     for row in puzzle_input:
-        if is_valid(row):
+        if validator(row):
             count += 1
     return count
 
-def is_valid(row):
+def no_duplicates(row):
     return len(row) == len(set(row))
+
+def no_anagrams(row):
+    sorted_letters = [str.join('', sorted(word)) for word in row]
+    return no_duplicates(sorted_letters)
+
 
 def read_input(filename):
     with open(filename) as f:
@@ -33,7 +51,10 @@ def read_input(filename):
 def main():
     puzzle_input = read_input('input4.txt')
 
-    result = count_passphrases(puzzle_input)
+    result = count_passphrases(puzzle_input, validator=no_duplicates)
+    print(f'Part 1 solution: {result}')
+
+    result = count_passphrases(puzzle_input, validator=no_anagrams)
     print(f'Part 1 solution: {result}')
 
 
--- advent_of_code_2017/test_day4.py
+++ advent_of_code_2017/test_day4.py
@@ -1,6 +1,13 @@
-from day4 import is_valid
+from day4 import no_duplicates, no_anagrams
 
 def test_examples():
-    assert True is is_valid('aa bb cc dd ee')
-    assert False is is_valid('aa bb cc dd aa')
-    assert True is is_valid('aa bb cc dd aaa')
+    assert True is no_duplicates('aa bb cc dd ee')
+    assert False is no_duplicates('aa bb cc dd aa')
+    assert True is no_duplicates('aa bb cc dd aaa')
+
+def test_examples_part2():
+    assert True is no_anagrams('abcde fghij')
+    assert False is no_anagrams('abcde xyz ecdab')
+    assert True is no_anagrams('a ab abc abd abf abj')
+    assert True is no_anagrams('iiii oiii ooii oooi oooo')
+    assert False is no_anagrams('oiii ioii iioi iiio')
