1fda4565295ebdab9f53f0aed485b4ff4995b7b6
codebicycle <codebicycle@gmail.com>
2017-12-06 23:36:53 +0200

    AoC 2017, Day 4: High-Entropy Passphrases - Fix tests

--- advent_of_code_2017/test_day4.py
+++ advent_of_code_2017/test_day4.py
@@ -1,13 +1,13 @@
 from day4 import no_duplicates, no_anagrams
 
 def test_examples():
-    assert True is no_duplicates('aa bb cc dd ee')
-    assert False is no_duplicates('aa bb cc dd aa')
-    assert True is no_duplicates('aa bb cc dd aaa')
+    assert True is no_duplicates('aa bb cc dd ee'.split())
+    assert False is no_duplicates('aa bb cc dd aa'.split())
+    assert True is no_duplicates('aa bb cc dd aaa'.split())
 
 def test_examples_part2():
-    assert True is no_anagrams('abcde fghij')
-    assert False is no_anagrams('abcde xyz ecdab')
-    assert True is no_anagrams('a ab abc abd abf abj')
-    assert True is no_anagrams('iiii oiii ooii oooi oooo')
-    assert False is no_anagrams('oiii ioii iioi iiio')
+    assert True is no_anagrams('abcde fghij'.split())
+    assert False is no_anagrams('abcde xyz ecdab'.split())
+    assert True is no_anagrams('a ab abc abd abf abj'.split())
+    assert True is no_anagrams('iiii oiii ooii oooi oooo'.split())
+    assert False is no_anagrams('oiii ioii iioi iiio'.split())
