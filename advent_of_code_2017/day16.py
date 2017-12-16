42a9e7228bf3af461ea6d5fd9bc07c87e154a3ad
codebicycle <codebicycle@gmail.com>
2017-12-16 13:07:55 +0200

    Aoc 2017, Day 16: Permutation Promenade - part 2
    spin, exchange, partner functions modify state argument

--- advent_of_code_2017/day16.py
+++ advent_of_code_2017/day16.py
@@ -18,6 +18,18 @@ After finishing their dance, the programs end up in order baedc.
 
 You watch the dance for a while and record their dance moves (your puzzle input). In what order are the programs standing after their dance?
 
+--- Part Two ---
+Now that you're starting to get a feel for the dance moves, you turn your attention to the dance as a whole.
+
+Keeping the positions they ended up in from their previous dance, the programs perform it again and again: including the first dance, a total of one billion (1000000000) times.
+
+In the example above, their second dance would begin with the order baedc, and use the same dance moves:
+
+s1, a spin of size 1: cbaed.
+x3/4, swapping the last two programs: cbade.
+pe/b, swapping programs e and b: ceadb.
+In what order are the programs standing after their billion dances?
+
 """
 import re
 
@@ -44,19 +56,15 @@ def parse(instruction):
     raise ValueError(f'Unknown instruction {instruction}.')
 
 def spin(number, state):
-    return state[-number:] + state[:-number]
+    state[:number], state[number:] = state[-number:], state[:-number]
 
 def exchange(position_a, position_b, state):
-    new_state = state.copy()
-    new_state[position_a] = state[position_b]
-    new_state[position_b] = state[position_a]
-    return new_state
+    state[position_a], state[position_b] = state[position_b], state[position_a]
 
 def partner(program_a, program_b, state):
     position_a = state.index(program_a)
     position_b = state.index(program_b)
-    new_state = state.copy()
-    return exchange(position_a, position_b, state)
+    exchange(position_a, position_b, state)
 
 MAPPING = {'s': spin,
            'x': exchange,
@@ -66,16 +74,12 @@ def interpret_instruction(parsed_instruction, state):
     instruction_code = parsed_instruction[0]
     arguments = parsed_instruction[1:]
     function = MAPPING[instruction_code]
-    new_state = function(*arguments, state=state)
-    return new_state
+    function(*arguments, state=state)
 
-def interpret(instructions):
-    state_str = 'abcdefghijklmnop'
-    state = list(state_str)
+def interpret(instructions, state):
     for instruction in instructions:
         parsed_instruction = parse(instruction)
-        state = interpret_instruction(parsed_instruction, state)
-    return ''.join(state)
+        interpret_instruction(parsed_instruction, state)
 
 def read_input(filename):
     with open(filename) as f:
@@ -83,15 +87,36 @@ def read_input(filename):
 
     return instructions
 
+def loop_length(instructions, initial_state):
+    state = initial_state.copy()
+    count = 0
+    while True:
+        count += 1
+        interpret(instructions, state)
+        if state == initial_state:
+            break
+    return count
+
+def repeat(instructions, times, initial_state):
+    state = initial_state.copy()
+    for _ in range(times):
+        interpret(instructions, state)
+    return state
+
 def main():
     instructions = read_input('input16.txt')
+    initial_state = list('abcdefghijklmnop')
 
-    result = interpret(instructions)
+    final_state = repeat(instructions, times=1, initial_state=initial_state)
+    result = ''.join(final_state)
     print('Part 1 solution:', result)
 
-    # result =
-    # print('Part 2 solution:', result)
-
+    loop = loop_length(instructions, initial_state)
+    times = 1_000_000_000
+    times = times % loop
+    final_state = repeat(instructions, times, initial_state)
+    result = ''.join(final_state)
+    print('Part 2 solution:', result)
 
 if __name__ == '__main__':
     main()
