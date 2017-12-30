cfc261f8489e5096bdeadbb23b7c397353a7391b
codebicycle <codebicycle@gmail.com>
2017-12-30 17:31:43 +0200

    AoC 2017, Day 22 - Fix tests

--- advent_of_code_2017/test_day22.py
+++ advent_of_code_2017/test_day22.py
@@ -1,35 +1,41 @@
-from day22 import Grid
+import pytest
+
+from day22 import Virus, build_grid
 
 
-def test_grid_turn_left():
-    grid = Grid()
-    assert grid.NORTH == grid.direction
-
-    grid._turn_left()
-    assert grid.WEST == grid.direction
-
-    grid._turn_left()
-    assert grid.SOUTH == grid.direction
-
-    grid._turn_left()
-    assert grid.EAST == grid.direction
-
-    grid._turn_left()
-    assert grid.NORTH == grid.direction
+@pytest.fixture
+def virus():
+    grid = build_grid(['.'])
+    return Virus(grid)
 
 
-def test_grid_turn_right():
-    grid = Grid()
-    assert grid.NORTH == grid.direction
+def test_at_first_virus_is_pointing_north(virus):
+    assert virus.NORTH == virus.direction
 
-    grid._turn_right()
-    assert grid.EAST == grid.direction
 
-    grid._turn_right()
-    assert grid.SOUTH == grid.direction
+def test_virus_turn_left(virus):
+    virus._turn_left()
+    assert virus.WEST == virus.direction
 
-    grid._turn_right()
-    assert grid.WEST == grid.direction
+    virus._turn_left()
+    assert virus.SOUTH == virus.direction
 
-    grid._turn_right()
-    assert grid.NORTH == grid.direction
+    virus._turn_left()
+    assert virus.EAST == virus.direction
+
+    virus._turn_left()
+    assert virus.NORTH == virus.direction
+
+
+def test_virus_turn_right(virus):
+    virus._turn_right()
+    assert virus.EAST == virus.direction
+
+    virus._turn_right()
+    assert virus.SOUTH == virus.direction
+
+    virus._turn_right()
+    assert virus.WEST == virus.direction
+
+    virus._turn_right()
+    assert virus.NORTH == virus.direction
