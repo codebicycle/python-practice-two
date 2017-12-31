b6dc5642604e28caf2d5c2522120554cb933bc79
codebicycle <codebicycle@gmail.com>
2017-12-31 10:13:47 +0200

    Fix balanced binary search tree rigid test

--- test_ballanced_bst.py
+++ test_ballanced_bst.py
@@ -1,16 +1,15 @@
+import math
+
 from balanced_bst import build_balanced_tree
 
 
 def test_ballanced_tree_from_collection():
     ordered = range(3, 13)
     bst = build_balanced_tree(ordered)
-    assert 7 == bst.root.value
-    assert 4 == bst.root.left.value
-    assert 10 == bst.root.right.value
-    assert 3 == bst.root.left.left.value
-    assert 5 == bst.root.left.right.value
-    assert 6 == bst.root.left.right.right.value
-    assert 8 == bst.root.right.left.value
-    assert 9 == bst.root.right.left.right.value
-    assert 11 == bst.root.right.right.value
-    assert 12 == bst.root.right.right.right.value
+    max_height = math.log(len(ordered), 2)
+    assert bst.height() <= max_height
+
+    ordered = range(25_579)
+    bst = build_balanced_tree(ordered)
+    max_height = math.log(len(ordered), 2)
+    assert bst.height() <= max_height
