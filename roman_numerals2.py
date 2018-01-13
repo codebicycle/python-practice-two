04a1997b4fb3e495a588cb1b75219a1683dbc53c
codebicycle <codebicycle@gmail.com>
2018-01-13 19:52:30 +0200

    Pass invalid roman numeral representation test

--- roman_numerals2.py
+++ roman_numerals2.py
@@ -1,5 +1,21 @@
+import re
+
+
 class OutOfRangeError(ValueError): pass
 class NotIntegerError(ValueError): pass
+class InvalidRomanNumeralError(ValueError): pass
+
+ROMAN_NUMERAL_PATTERN = re.compile('''
+    ^                   # beginning of string
+    M{0,3}              # thousands - 0 to 3 Ms
+    (CM|CD|D?C{0,3})    # hundreds - 900 (CM), 400 (CD), 0-300 (0 to 3 Cs),
+                        #            or 500-800 (D, followed by 0 to 3 Cs)
+    (XC|XL|L?X{0,3})    # tens - 90 (XC), 40 (XL), 0-30 (0 to 3 Xs),
+                        #        or 50-80 (L, followed by 0 to 3 Xs)
+    (IX|IV|V?I{0,3})    # ones - 9 (IX), 4 (IV), 0-3 (0 to 3 Is),
+                        #        or 5-8 (V, followed by 0 to 3 Is)
+    $                   # end of string
+    ''', re.VERBOSE)
 
 roman_numeral_map = (
     ('M', 1000),
@@ -38,6 +54,9 @@ def to_roman(n):
 
 def from_roman(roman_numeral):
     """Convert roman to arab numeral."""
+    if not ROMAN_NUMERAL_PATTERN.search(roman_numeral):
+        raise InvalidRomanNumeralError('Invalid roman numeral.')
+
     result = 0
     index = 0
     while index < len(roman_numeral):
