5d09c77c9eda8c60339a7e72474e7628dbaf9a3e
codebicycle <codebicycle@gmail.com>
2018-01-15 23:07:31 +0200

    Refactor all pairs shortest path
    Change order
    Add docstrings

--- algorithms_heineman/apsp.py
+++ algorithms_heineman/apsp.py
@@ -1,7 +1,7 @@
 """
 All Pairs Shortest Path
 
-Use Dynammic Programming to determine the shortest accumulated totat of a path
+Use Dynamic Programming to determine the shortest accumulated total of a path
 between any two vertices in a weighted graph.
 
 The code uses a graph representation such as the following:
@@ -15,12 +15,37 @@ graph = {
 }
 
 A dictionary where keys are the vertices and the values are
-dicts of vertex: weight pairs.
+dictionaries of vertex: weight pairs.
 """
 
-from pprint import pprint
+def shortest_path(graph, start, finish):
+    """Return the shortest path and its cost, as a tuple, between the given vertices
+    start and finish from the graph.
+    """
+    distance, predecessors = all_pairs_shortest_path(graph)
+    path = _build_path(predecessors, start, finish)
+    return path, distance[start, finish]
 
-def build_matrices(graph):
+def all_pairs_shortest_path(graph):
+    """Compute the shortest path between all pairs of vertices from the given graph
+    Return distance and predecessors as a tuple.
+    Distance is a matrix with the costs of the shortest paths.
+    Predecessors is a matrix that keeps track of the preceding vertex from the shortest path.
+    """
+    distance, predecessors = _build_matrices(graph)
+
+    for vertex in graph:
+        for pair in distance:
+            if vertex in pair:
+                continue
+            v1, v2 = pair
+            cost_through_vertex = distance[v1, vertex] + distance[vertex, v2]
+            if distance[pair] > cost_through_vertex:
+                distance[pair] = cost_through_vertex
+                predecessors[pair] = predecessors[vertex, v2]
+    return distance, predecessors
+
+def _build_matrices(graph):
     """Build adjacency matrix and predecessors matrix"""
     adjacency = {}
     predecessors = {}
@@ -39,32 +64,14 @@ def build_matrices(graph):
             predecessors[v1, v2] = predecessor
     return adjacency, predecessors
 
-def shortest_path(graph, start, finish):
-    distance, predecessors = all_pairs_shortest_path(graph)
-    path = _build_path(predecessors, start, finish)
-    return distance[start, finish], path
-
 def _build_path(predecessors, start, finish):
+    """Construct a path from vertex start to finish given a predecessor matrix"""
     path_backwards = []
     predecessor = finish
     while predecessor is not None:
         path_backwards.append(predecessor)
         predecessor = predecessors[start, predecessor]
     return list(reversed(path_backwards))
-
-def all_pairs_shortest_path(graph):
-    distance, predecessors = build_matrices(graph)
-
-    for vertex in graph:
-        for pair in distance:
-            if vertex in pair:
-                continue
-            v1, v2 = pair
-            cost_through_vertex = distance[v1, vertex] + distance[vertex, v2]
-            if distance[pair] > cost_through_vertex:
-                distance[pair] = cost_through_vertex
-                predecessors[pair] = predecessors[vertex, v2]
-    return distance, predecessors
 
 
 def main():
@@ -78,7 +85,7 @@ def main():
 
     start = 2
     finish = 3
-    cost, path = shortest_path(graph, start, finish)
+    path, cost = shortest_path(graph, start, finish)
     print(f'The shortest path (lowest cost path) between vertices {start} and {finish} is {path}'
           f' and its cost is {cost}.')
 
--- algorithms_heineman/test_apsp.py
+++ algorithms_heineman/test_apsp.py
@@ -13,21 +13,21 @@ def graph():
     }
 
 def test_shortest_path_direct_arc(graph):
-    cost, path = shortest_path(graph, 0, 1)
+    path, cost = shortest_path(graph, 0, 1)
     assert cost == 2
     assert path == [0, 1]
 
 def test_shortest_path_indirect(graph):
-    cost, path = shortest_path(graph, 0, 2)
+    path, cost = shortest_path(graph, 0, 2)
     assert cost == 5
     assert path == [0, 1, 2]
 
 def test_shortest_path_indirect_vs_direct_arc(graph):
-    cost, path = shortest_path(graph, 2, 3)
+    path, cost = shortest_path(graph, 2, 3)
     assert cost == 4
     assert path == [2, 4, 3]
 
 def test_shortest_path_longer_path(graph):
-    cost, path = shortest_path(graph, 2, 0)
+    path, cost = shortest_path(graph, 2, 0)
     assert cost == 12
     assert path == [2, 4, 3, 0]
