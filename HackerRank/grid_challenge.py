f9ff9ec8bb8590cda96d46644623cc6549f4284b
codebicyle <codebicycle@gmail.com>
2021-11-17 15:01:05 +0200

    Add grid_challenge.py

---
+++ HackerRank/grid_challenge.py
@@ -0,0 +1,28 @@
+def gridChallenge(grid):
+    grid = [sorted(row) for row in grid]
+    transposed = transpose(grid)
+    return 'YES' if is_sorted(transposed) else 'NO'
+
+
+def transpose(arr):
+    transposed = []
+    for idx in range(len(arr)):
+        new_row = ''
+        for row in arr:
+            new_row += row[idx]
+        transposed.append(new_row)
+    return transposed
+
+
+def is_sorted(grid):
+    for row in grid:
+        if row != ''.join(sorted(row)):
+            return False
+    return True
+
+
+if __name__ == '__main__':
+    grid = ['abc', 'ade', 'efg']
+    expected = "YES"
+    result = gridChallenge(grid)
+    assert result == expected
